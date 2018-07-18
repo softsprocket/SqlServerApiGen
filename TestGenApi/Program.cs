62f82dec0846834f12d2dba1077b0110572fbef2
Greg Martin <greg@softsprocket.com>
2018-07-18 15:23:08 -0700

    initial commit

--- TestGenApi/Program.cs
+++ TestGenApi/Program.cs
@@ -11,8 +11,6 @@ namespace TestGenApi
     {
         static void Main(string[] args)
         {
-            //string connectionString = "Server = DESKTOP-T0L66I3; Trusted_Connection = True; Database = multivista";
-            //string connectionString = "Server = 10.171.239.112; User = sa; Password = vista1; Database = Multivista";
 
             string connectionString = "Server = localhost; User = sa; Password = vista1; Database = Multivista";
             Multivista multivista = new Multivista(connectionString);
